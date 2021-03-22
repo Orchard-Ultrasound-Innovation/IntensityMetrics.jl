18e03b268d6d755a60558ede5c02cb4b79fef61c
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2021-03-22 11:31:23 -0700

    Auto-formater formated the docs Makefile

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,27 @@
 using IntensityMetrics
 using Documenter
 
-DocMeta.setdocmeta!(IntensityMetrics, :DocTestSetup, :(using IntensityMetrics); recursive=true)
+DocMeta.setdocmeta!(
+    IntensityMetrics,
+    :DocTestSetup,
+    :(using IntensityMetrics);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[IntensityMetrics],
-    authors="Morten F. Rasmussen <10264458+mofii@users.noreply.github.com> and contributors",
-    repo="https://github.com/Orchard-Ultrasound-Innovation/IntensityMetrics.jl/blob/{commit}{path}#{line}",
-    sitename="IntensityMetrics.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Orchard-Ultrasound-Innovation.github.io/IntensityMetrics.jl",
-        assets=String[],
+    modules = [IntensityMetrics],
+    authors = "Morten F. Rasmussen <10264458+mofii@users.noreply.github.com> and contributors",
+    repo = "https://github.com/Orchard-Ultrasound-Innovation/IntensityMetrics.jl/blob/{commit}{path}#{line}",
+    sitename = "IntensityMetrics.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://Orchard-Ultrasound-Innovation.github.io/IntensityMetrics.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
 deploydocs(;
-    repo="github.com/Orchard-Ultrasound-Innovation/IntensityMetrics.jl",
+    repo = "github.com/Orchard-Ultrasound-Innovation/IntensityMetrics.jl",
     devbranch = "main",
 )
