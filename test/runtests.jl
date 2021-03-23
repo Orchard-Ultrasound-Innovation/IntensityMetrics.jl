8c78f2efcdeee8d722c65f584ca10ffdb3324c20
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2021-03-23 00:17:50 -0700

    Started on tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
-using IntensityMetrics
 using Test
+using SafeTestsets
+using TestSetExtensions
+
+
 
 @testset "IntensityMetrics.jl" begin
-    # Write your tests here.
+    @testset ExtendedTestSet "intensity tests" begin
+            using ..IntensityMetrics
+            using Unitful
+
+            f0 = 5e6u"Hz";
+            fs = 40e6u"Hz";
+            pulse_duration = 10u"µs";
+            dt = promote(1/fs, 1u"s")[1];
+            num_samples = ceil(pulse_duration/dt)
+            M = Medium();
+            E = Excitation();
+
+            t = collect(range(0,num_samples-1) *dt)
+            data = sin.(2*pi*f0*t)u"MPa";
+
+            I = intensity(data, M)
+            sppa = intensity_sppa(data, M, E)
+            spta = intensity_spta(data, M, E)
+    end
+
 end
