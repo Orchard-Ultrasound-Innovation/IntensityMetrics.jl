9e1ae64aa3959a0c4a9575c342826a1b79967ffe
Esti Gajda <egajda@umich.edu>
2021-04-07 16:41:53 -0400

    Remove unused import

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,24 @@
 using Test
-using SafeTestsets
 using TestSetExtensions
 using IntensityMetrics
 using IntensityMetrics.Unitful
 
-
-
 @testset "IntensityMetrics.jl" begin
     @testset ExtendedTestSet "intensity tests" begin
-            f0 = 5e6u"Hz"
-            fs = 40e6u"Hz"
-            pulse_duration = 10u"µs"
-            dt = promote(1/fs, 1u"s")[1];
-            num_samples = ceil(pulse_duration/dt)
-            M = Medium();
-            E = Excitation();
+        f0 = 5e6u"Hz"
+        fs = 40e6u"Hz"
+        pulse_duration = 10u"µs"
+        dt = promote(1/fs, 1u"s")[1];
+        num_samples = ceil(pulse_duration/dt)
+        M = Medium();
+        E = Excitation();
 
-            t = collect(range(0,num_samples-1; step=1) *dt)
-            data = sin.(2*pi*f0*t)u"MPa";
+        t = collect(range(0,num_samples-1; step=1) *dt)
+        data = sin.(2*pi*f0*t)u"MPa";
 
-            I = map(x->intensity(x, M), data)
-            sppa = intensity_sppa(data, M, E)
-            spta = intensity_spta(data, M, E)
+        I = map(x->intensity(x, M), data)
+        sppa = intensity_sppa(data, M, E)
+        spta = intensity_spta(data, M, E)
     end
 
 end
