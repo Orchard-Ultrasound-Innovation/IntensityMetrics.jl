1d0caefe6dff63ff967f043a789e8df4cd38f4cf
Esti Gajda <egajda@umich.edu>
2021-04-07 14:50:56 -0400

    Use calculations from IntensityScans

--- src/IntensityMetrics.jl
+++ src/IntensityMetrics.jl
@@ -1,11 +1,17 @@
 module IntensityMetrics
 
 using Unitful
-Unitful.promote_to_derived() #Defines promotion rules to use derived SI units in promotion for common dimensions of quantities
 
 export Medium, Excitation
 export intensity, intensity_sppa, intensity_spta, mechanical_index
 export peak_np, peak_pp, peak_ptp
+
+const PressureArray{x} = Array{T, x} where T <: Union{Unitful.Pressure, Number}
+
+squeeze(A::PressureArray{2}) = A[1, :]
+squeeze(A::PressureArray{3}) = A[1, :, :]
+squeeze(A::PressureArray{4}) = A[1, :, :, :]
+
 
 
 const volt = typeof(1.0u"V")
@@ -14,8 +20,6 @@ const m_per_s = typeof(1.0u"m" / 1.0u"s")
 const seconds = typeof(1.0u"s")
 const herz = typeof(1.0u"Hz")
 const pascal = typeof(1.0u"Pa")
-
-#include("models.jl")
 
 """
 struct Medium
@@ -43,6 +47,7 @@ struct Excitation
     duty_cycle::Float64     # [0,1]
     total_duration::seconds
 end
+
 Excitation() = Excitation(5e6u"Hz", 0.1u"s", 1.0, 10u"s")
 Excitation(f0, pd::Number, dc) = Excitation(f0, Float64(pd), dc, 10)
 
@@ -58,8 +63,10 @@ function intensity(p::Unitful.Pressure, M::Medium)
     return format_intensity(p^2 / (M.density * M.c))
 end
 
+"""
 intensity(p_vec::Vector{T}, M::Medium) where {T <: Unitful.Pressure} =
     map(p -> intensity(p, M), p_vec)  # W/m²
+"""
 # Unitless version
 #intensity(p::T, M::Medium) where {T :< Number} = p^2 / ustrip(M.ρ * M.c)
 #intensity(p_vec::Vector{T}, M::Medium) where {T :< Number} = map(p_vec->intensity(p_vec,M),p_vec)  # W/m²
@@ -71,11 +78,19 @@ intensity(p_vec::Vector{T}, M::Medium) where {T <: Unitful.Pressure} =
     Spatial Peak, Pulse Averaged Intensity.
     p is expected to be a vector/time series containing only the measured pressure from the excitation pulse.
     Output unit is W/cm².
-"""
+
 function intensity_sppa(p::Vector{T}, M::Medium, E::Excitation) where {T<:Unitful.Pressure}
     scaling = 1u"s"/(E.pulse_duration * 10_000u"cm^2/m^2");
     integrated = mapreduce(p -> intensity(p, M), +, p);  # W/cm²
     return integrated * scaling  # W/cm²
+end
+"""
+
+function intensity_sppa(pressure, medium, excitation)
+    intensities = u"s" * mapreduce(
+        p -> intensity(p, medium), +, pressure; dims=1
+    ) 
+    return squeeze(intensities / (excitation.pulse_duration * 10_000u"cm^2/m^2"))
 end
 
 
@@ -84,10 +99,12 @@ end
     Spatial Peak, Time Averaged Intensity.
     p is expected to be a vector/time series containing only the measured pressure from the excitation pulse.
     Output unit is W/cm².
-"""
 function intensity_spta(p::Vector{T}, M::Medium, E::Excitation) where {T<:Union{Unitful.Pressure,Number}}
     return intensity_sppa(p, M, E) * E.duty_cycle # W/cm²
 end
+"""
+
+intensity_spta(p, M, E) = intensity_sppa(p, M, E) * E.duty_cycle # W/cm²
 
 
 """
@@ -103,29 +120,36 @@ end
     mechanical_index(p::Unitful.Pressure, E::Excitation)
     p is expected to be a vector/time series containing the measured pressure from the excitation pulse.
     Output is dimensionless.
-"""
 mechanical_index(p::Vector{T}, E::Excitation) where {T<:Unitful.Pressure} =
     peak_np(p / 1u"MPa") / sqrt(E.f0 / 1u"MHz")
+"""
+function mechanical_index(p::Vector{T}, E::Excitation) where {T<:Unitful.Pressure}
+    peak_rarefaction_pressure = peak_np(uconvert.(u"MPa", p))
+    center_frequency = sqrt(uconvert(u"MHz", E.f0))
+    return ustrip(peak_rarefaction_pressure / center_frequency)
+end
+
 
 """
     peak_np(p)
     Peak Negative Pressure.
 """
-peak_np(p::Unitful.Pressure) = abs(minimum(p))            # Pa
-
+peak_np(p::Vector{T}) where {T<:Unitful.Pressure}= abs(minimum(p)) # Pa
 
 """
     peak_pp(p)
     Peak Positive Pressure.
     Output unit is unchanged, Pa.
 """
-peak_pp(p::Unitful.Pressure) = abs(maximum(p))            # Pa
+peak_pp(p::Vector{T}) where {T<:Unitful.Pressure} = abs(maximum(p)) # Pa
 
 """
     peak_ptp(p)
     peak pressure measured in peak-to-peak.
     Output unit is unchanged, Pa.
 """
-peak_ptp(p::Unitful.Pressure) = peak_pp(p) + peak_np(p)  #Pa
+peak_ptp(p::Vector{T}) where {T<:Unitful.Pressure} =
+    peak_pp(p) + peak_np(p)  #Pa
+
 
 end
