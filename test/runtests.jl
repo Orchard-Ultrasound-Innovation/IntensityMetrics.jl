ed62310540408329de8601c9cbfc5acf9ea65b09
Steve Leung <steve@orchardultrasound.com>
2021-05-03 14:49:08 -0700

    Created initial unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,33 @@ using TestSetExtensions
 using IntensityMetrics
 using IntensityMetrics.Unitful
 
-@testset "IntensityMetrics.jl" begin
-    @testset ExtendedTestSet "intensity tests" begin
-        f0 = 5e6u"Hz"
-        fs = 40e6u"Hz"
-        pulse_duration = 10u"µs"
-        dt = promote(1/fs, 1u"s")[1];
-        num_samples = ceil(pulse_duration/dt)
-        M = Medium();
-        E = Excitation();
+@testset "perfect sinusoid" begin
+    # ultrasound parameters
+    center_freq = 5e6u"Hz"
+    pulse_duration = 10u"µs"
+    pressure_amplitude = 1u"MPa"
+    duty_cycle = 1.0
+    M = Medium();
+    E = Excitation(center_freq, pulse_duration, duty_cycle);
 
-        t = collect(range(0,num_samples-1; step=1) *dt)
-        data = sin.(2*pi*f0*t)u"MPa";
+    # waveform parameters
+    sampling_freq = 40e6u"Hz"
+    dt = 1/sampling_freq
+    num_samples = ceil(pulse_duration/dt)
+    time = collect((0:num_samples-1)*dt)
+    data = sin.(2*pi*center_freq*time)*pressure_amplitude
+    
+    # intensity tests
+    intensity_expected = uconvert(u"W/m^2", pressure_amplitude^2 / (M.density*M.c))
+    @test intensity(pressure_amplitude, M) ≈ intensity_expected atol=0.001u"W/m^2"
+    @test intensity(uconvert(u"Pa", pressure_amplitude), M) ≈ intensity_expected atol=0.001u"W/m^2"   
 
-        I = map(x->intensity(x, M), data)
-        sppa = intensity_sppa(data, M, E)
-        spta = intensity_spta(data, M, E)
-    end
+    # intensity_sppa tests
+    intensity_sppa_expected = uconvert(u"W/m^2", pressure_amplitude^2 / (2*M.density*M.c))
+    @test intensity_sppa(data, M, E) ≈ intensity_sppa_expected atol=0.001u"W/m^2"
 
+    # mechanical_index tests
+    mechanical_index_expected = ustrip(uconvert(u"MPa", pressure_amplitude) / sqrt(uconvert(u"MHz", center_freq)))
+    @test mechanical_index(data, E) ≈ mechanical_index_expected atol=0.001
+    @test mechanical_index(uconvert(u"Pa", data), E) ≈ mechanical_index_expected atol=0.001
 end
